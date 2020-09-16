3da2fb808501eaba0420680b2a3fd50699fa2b37
fly2moon2 <fly2moon2@gmail.com>
2020-09-16 21:17:20 +0800

    rocket json

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,82 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
-extern crate rocket_contrib;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
+use std::sync::Mutex;
 use std::collections::HashMap;
 
-use rocket::response::content::Json;
+// ref:
+// https://github.com/SergioBenitez/Rocket/blob/v0.4/examples/json/src/main.rs
+//use rocket::response::content::Json;
+use rocket_contrib::json::{Json,JsonValue};
+use rocket::State;
 
 // Note: rocket_contrib::templates::Template version 2018 not rocket_contrib::Template
 use rocket_contrib::templates::Template;
 use rocket_contrib::serve::StaticFiles;
+
+
+// The type to represent the ID of a message.
+type MsgID = usize;
+
+// We're going to store all of the messages here. No need for a DB.
+type MessageMap = Mutex<HashMap<MsgID, String>>;
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    id: Option<MsgID>,
+    contents: String
+}
+
+// TODO: This example can be improved by using `route` with multiple HTTP verbs.
+//$curl -X POST -d '{ "contents": "Hello, world!"}' -H 'Content-Type: application/json' http:/localhost:8000/message/1
+#[post("/<id>", format = "json", data = "<message>")]
+fn new(id: MsgID, message: Json<Message>, map: State<MessageMap>) -> JsonValue {
+    let mut hashmap = map.lock().expect("map lock.");
+    if hashmap.contains_key(&id) {
+        json!({
+            "status": "error",
+            "reason": "ID exists. Try put."
+        })
+    } else {
+        hashmap.insert(id, message.0.contents);
+        json!({ "status": "ok" })
+    }
+}
+
+// $curl -X PUT -d '{ "contents": "Harlo, wolf!"}' -H 'Content-Type: application/json' http:/localhost:8000/message/1
+#[put("/<id>", format = "json", data = "<message>")]
+fn update(id: MsgID, message: Json<Message>, map: State<MessageMap>) -> Option<JsonValue> {
+    let mut hashmap = map.lock().unwrap();
+    if hashmap.contains_key(&id) {
+        hashmap.insert(id, message.0.contents);
+        Some(json!({ "status": "ok" }))
+    } else {
+        None
+    }
+}
+
+//$curl http://localhost:8000/message/1
+#[get("/<id>", format = "json")]
+fn get(id: MsgID, map: State<MessageMap>) -> Option<Json<Message>> {
+    let hashmap = map.lock().unwrap();
+    hashmap.get(&id).map(|contents| {
+        Json(Message {
+            id: Some(id),
+            contents: contents.clone()
+        })
+    })
+}
+
+#[catch(404)]
+fn not_found() -> JsonValue {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
+}
 
 // ref:
 // https://medium.com/@james_32022/rocket-frontend-templates-and-static-assets-5b6d04243a08
@@ -78,5 +145,8 @@ fn main() {
     .mount("/static", StaticFiles::from("static"))
     .mount("/",routes![index, jackson, wave, template, branch::world])
     .attach(Template::fairing())
+    .mount("/message", routes![new, update, get])
+    .register(catchers![not_found])
+    .manage(Mutex::new(HashMap::<MsgID, String>::new()))
     .launch();
 }
